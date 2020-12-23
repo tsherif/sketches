83a0b11217aab688c0d50ddf023b7c1fd32ef11e
Tarek Sherif <tareksherif@pm.me>
2020-12-23 06:52:24 -0500

    Window options

---
+++ 2020-12/23/compile.bat
@@ -0,0 +1,1 @@
+cl /Zi /D "_UNICODE" /D "UNICODE" window-options.cpp user32.lib
---
+++ 2020-12/23/window-options.cpp
@@ -0,0 +1,60 @@
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+
+const WCHAR WIN_CLASS_NAME[] = L"CREATEWINDOW_CLASS"; 
+
+LRESULT CALLBACK winProc(HWND window, UINT message, WPARAM wParam, LPARAM lParam) {
+	switch (message) {
+		case WM_CLOSE: {
+			PostQuitMessage(0);
+			return 0;
+		} break;
+	}
+
+	return DefWindowProc(window, message, wParam, lParam);
+}
+
+int CALLBACK WinMain(HINSTANCE instance, HINSTANCE, LPSTR cmdLine, int show) {
+	WNDCLASSEX winClass = {};
+	winClass.cbSize = sizeof(winClass);
+	winClass.style = CS_HREDRAW | CS_VREDRAW;
+	winClass.lpfnWndProc = winProc;
+	winClass.hInstance = instance;
+	winClass.hIcon = LoadIcon(instance, IDI_APPLICATION);
+	winClass.hIconSm = LoadIcon(instance, IDI_APPLICATION);
+	winClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	winClass.hbrBackground = (HBRUSH)(COLOR_WINDOW + 2);
+	winClass.lpszClassName = WIN_CLASS_NAME;
+
+	if (!RegisterClassEx(&winClass)) {
+		MessageBox(NULL, L"Failed to register window class!", L"FAILURE", MB_OK);
+
+		return 1;
+	}
+
+	HWND window = CreateWindow(
+		WIN_CLASS_NAME,
+		L"We made a window!",
+		WS_OVERLAPPEDWINDOW | WS_VISIBLE | WS_VSCROLL,
+		CW_USEDEFAULT, CW_USEDEFAULT,
+		500, 500,
+		NULL, 
+		NULL,
+		instance,
+		NULL
+	);
+
+	if (!window) {
+		MessageBox(NULL, L"Failed to create window!", L"FAILURE", MB_OK);
+
+		return 1;
+	}
+
+	MSG message;
+	while (GetMessage(&message, NULL, 0, 0) > 0) {
+		TranslateMessage(&message);
+		DispatchMessage(&message);
+	}
+
+	return (int) message.wParam;
+}
