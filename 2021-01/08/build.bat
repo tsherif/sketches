5d8dce762ae473cf1a16df5d2564b35944db0adf
Tarek Sherif <tareksherif@pm.me>
2021-01-08 05:40:59 -0500

    Test latest sogl

---
+++ 2021-01/08/build.bat
@@ -0,0 +1,1 @@
+cl /Zi /D "_UNICODE" /D "UNICODE" opengl-win32.cpp simple-opengl-loader-win32.c utils.cpp user32.lib gdi32.lib opengl32.lib
---
+++ 2021-01/08/opengl-win32.cpp
@@ -0,0 +1,320 @@
+#define WIN32_LEAN_AND_MEAN
+#define _USE_MATH_DEFINES
+#define SOGL_IMPLEMENTATION
+#include <windows.h>
+#include "sogl.h"
+#include "wglext.h"
+#include <cstdint>
+#include <cmath>
+#include <cstdio>
+
+/*
+    References:
+        https://www.khronos.org/opengl/wiki/Creating_an_OpenGL_Context_(WGL)
+        https://www.khronos.org/opengl/wiki/Load_OpenGL_Functions
+        https://www.khronos.org/registry/OpenGL/extensions/ARB/WGL_ARB_pixel_format.txt
+        https://www.khronos.org/registry/OpenGL/extensions/ARB/ARB_multisample.txt
+        https://mariuszbartosik.com/opengl-4-x-initialization-in-windows-without-a-framework/
+*/
+
+// Check that GL loads in separate file.
+void setUniform(GLuint location, float f);
+
+#define DECLARE_WGL_EXT_FUNC(returnType, name, ...) typedef returnType (WINAPI *name##FUNC)(__VA_ARGS__);\
+    name##FUNC name = (name##FUNC)0;
+
+#define LOAD_WGL_EXT_FUNC(name) name = (name##FUNC) wglGetProcAddress(#name)
+
+/////////////////////////////////////
+// Set up OpenGL function pointers
+/////////////////////////////////////
+
+DECLARE_WGL_EXT_FUNC(BOOL, wglChoosePixelFormatARB, HDC hdc, const int *piAttribIList, const FLOAT *pfAttribFList, UINT nMaxFormats, int *piFormats, UINT *nNumFormats);
+DECLARE_WGL_EXT_FUNC(HGLRC, wglCreateContextAttribsARB, HDC hDC, HGLRC hshareContext, const int *attribList);
+DECLARE_WGL_EXT_FUNC(BOOL, wglSwapIntervalEXT, int interval);
+
+////////////////
+// WIN32 setup
+////////////////
+
+const WCHAR WIN_CLASS_NAME[] = L"OPENGL_WINDOW_CLASS"; 
+
+LRESULT CALLBACK winProc(HWND window, UINT message, WPARAM wParam, LPARAM lParam) {
+    switch (message) {
+        case WM_CLOSE: {
+            PostQuitMessage(0);
+            return 0;
+        } break;
+    }
+
+    return DefWindowProc(window, message, wParam, lParam);
+}
+
+int CALLBACK WinMain(HINSTANCE instance, HINSTANCE, LPSTR cmdLine, int showWindow) {
+    WNDCLASSEX winClass = {};
+    winClass.cbSize = sizeof(winClass);
+    winClass.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
+    winClass.lpfnWndProc = winProc;
+    winClass.hInstance = instance;
+    winClass.hIcon = LoadIcon(instance, IDI_APPLICATION);
+    winClass.hIconSm = LoadIcon(instance, IDI_APPLICATION);
+    winClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+    winClass.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    winClass.lpszClassName = WIN_CLASS_NAME;
+
+    if (!RegisterClassEx(&winClass)) {
+        MessageBox(NULL, L"Failed to register window class!", L"FAILURE", MB_OK);
+
+        return 1;
+    }
+
+    ////////////////////////////////////////////////////////////////////
+    // Create a dummy window so we can get WGL extension functions
+    ////////////////////////////////////////////////////////////////////
+
+    HWND dummyWindow = CreateWindow(WIN_CLASS_NAME, L"DUMMY", WS_OVERLAPPEDWINDOW, 0, 0, 1, 1, NULL,  NULL, instance, NULL);
+
+    if (!dummyWindow) {
+        MessageBox(NULL, L"Failed to create window!", L"FAILURE", MB_OK);
+
+        return 1;
+    }
+
+    HDC dummyContext = GetDC(dummyWindow);
+
+    PIXELFORMATDESCRIPTOR pfd = {};
+    pfd.nSize = sizeof(PIXELFORMATDESCRIPTOR);
+    pfd.nVersion = 1;
+    pfd.dwFlags = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER,
+    pfd.iPixelType = PFD_TYPE_RGBA,        
+    pfd.cColorBits = 32;                   
+    pfd.cDepthBits = 24;           
+    pfd.cStencilBits = 8;                 
+    pfd.iLayerType = PFD_MAIN_PLANE;
+    
+    int pixelFormat = ChoosePixelFormat(dummyContext, &pfd);
+    SetPixelFormat(dummyContext, pixelFormat, &pfd);
+    HGLRC dummyGL = wglCreateContext(dummyContext);
+    wglMakeCurrent(dummyContext, dummyGL);
+
+    LOAD_WGL_EXT_FUNC(wglChoosePixelFormatARB);
+    LOAD_WGL_EXT_FUNC(wglCreateContextAttribsARB);
+    LOAD_WGL_EXT_FUNC(wglSwapIntervalEXT);
+
+    if (!wglCreateContextAttribsARB || !wglCreateContextAttribsARB) {
+        MessageBox(NULL, L"Didn't get wgl ARB functions!", L"FAILURE", MB_OK);
+        return 1;
+    }
+
+    wglMakeCurrent(NULL, NULL);
+    wglDeleteContext(dummyGL);
+    DestroyWindow(dummyWindow);
+
+    /////////////////////////////////////////////
+    // Create real window and rendering context
+    /////////////////////////////////////////////
+
+    HWND window = CreateWindow(
+        WIN_CLASS_NAME,
+        L"WIN32 OPENGL!!!",
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT,
+        800, 800,
+        NULL, 
+        NULL,
+        instance,
+        NULL
+    );
+
+    if (!window) {
+        MessageBox(NULL, L"Failed to create window!", L"FAILURE", MB_OK);
+
+        return 1;
+    }
+
+    HDC deviceContext = GetDC(window);
+
+    const int pixelAttribList[] = {
+        WGL_DRAW_TO_WINDOW_ARB, GL_TRUE,
+        WGL_SUPPORT_OPENGL_ARB, GL_TRUE,
+        WGL_DOUBLE_BUFFER_ARB, GL_TRUE,
+        WGL_PIXEL_TYPE_ARB, WGL_TYPE_RGBA_ARB,
+        WGL_COLOR_BITS_ARB, 32,
+        WGL_DEPTH_BITS_ARB, 24,
+        WGL_STENCIL_BITS_ARB, 8,
+        WGL_ACCELERATION_ARB, WGL_FULL_ACCELERATION_ARB,
+        WGL_SAMPLE_BUFFERS_ARB, GL_TRUE,
+        WGL_SAMPLES_ARB, 4,
+        0
+    };
+
+    UINT numFormats;
+    BOOL success;
+    success = wglChoosePixelFormatARB(deviceContext, pixelAttribList, NULL, 1, &pixelFormat, &numFormats);
+
+    if (!success || numFormats == 0) {
+        MessageBox(NULL, L"Didn't get ARB pixel format!", L"FAILURE", MB_OK);
+        return 1;
+    }
+    
+    DescribePixelFormat(deviceContext, pixelFormat, sizeof(pfd), &pfd);
+    SetPixelFormat(deviceContext, pixelFormat, &pfd);
+
+    const int contextAttribList[] = {
+        WGL_CONTEXT_MAJOR_VERSION_ARB, 4,
+        WGL_CONTEXT_MINOR_VERSION_ARB, 5,
+        WGL_CONTEXT_PROFILE_MASK_ARB, WGL_CONTEXT_CORE_PROFILE_BIT_ARB,
+        0
+    };
+
+    HGLRC gl = wglCreateContextAttribsARB(deviceContext, NULL, contextAttribList);
+
+    if (!gl) {
+        MessageBox(NULL, L"Didn't get ARB GL context!", L"FAILURE", MB_OK);
+        return 1;
+    }
+
+    wglMakeCurrent(deviceContext, gl);
+    wglSwapIntervalEXT(1);
+    sogl_loadOpenGL();
+
+    char rendererString[256] = {};
+    sprintf(rendererString, "Win32 OpenGL - %s",  glGetString(GL_RENDERER));
+    SetWindowTextA(window, rendererString);
+
+    if (glMaxShaderCompilerThreadsKHR) {
+        GLint maxThreads = 0;
+        glMaxShaderCompilerThreadsKHR(4);
+        glGetIntegerv(GL_MAX_SHADER_COMPILER_THREADS_KHR, &maxThreads);
+        char buffer[256] = {};
+        sprintf(buffer, "Max shader compiler threads = %d!", maxThreads);
+        MessageBoxA(NULL, buffer, "SUCCESS", MB_OK);
+    }
+
+    ///////////////////////////
+    // Set up GL resources
+    ///////////////////////////
+
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+    const char* vsSource = R"GLSL(#version 450
+    layout (location=0) in vec2 position;
+    layout (location=1) in vec3 color;
+    
+    uniform float angle;
+
+    out vec3 vColor;
+    void main() {
+        float s = sin(angle);
+        float c = cos(angle);
+
+        mat2 rot = mat2(
+            c, s,
+            -s, c
+        );
+
+        vColor = color;
+        gl_Position = vec4(rot * position, 0.0, 1.0);
+    }
+    )GLSL";
+
+    const char* fsSource = R"GLSL(#version 450
+    in vec3 vColor;
+    out vec4 fragColor;
+    void main() {
+        fragColor = vec4(vColor, 1.0);
+    }
+    )GLSL";
+
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vsSource, NULL);
+    glCompileShader(vertexShader);
+
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fsSource, NULL);
+    glCompileShader(fragmentShader);
+
+    GLuint program = glCreateProgram();
+    glAttachShader(program, vertexShader);
+    glAttachShader(program, fragmentShader);
+    glLinkProgram(program);
+
+    GLint result;
+    glGetProgramiv(program, GL_LINK_STATUS, &result);
+
+    if (result != GL_TRUE) {
+        MessageBox(NULL, L"Program failed to link!", L"FAILURE", MB_OK);
+    }
+
+    glUseProgram(program);
+
+    GLuint triangleArray;
+    glGenVertexArrays(1, &triangleArray);
+    glBindVertexArray(triangleArray);
+
+    float positions[] = {
+        -0.5, -0.5,
+        0.5, -0.5,
+        0.0, 0.5
+    };
+
+    GLuint positionBuffer;
+    glGenBuffers(1, &positionBuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, positionBuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, NULL);
+    glEnableVertexAttribArray(0);
+
+    uint8_t colors[] = {
+        255, 0, 0,
+        0, 255, 0,
+        0, 0, 255
+    };
+
+    GLuint colorBuffer;
+    glGenBuffers(1, &colorBuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, colorBuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
+    glVertexAttribPointer(1, 3, GL_UNSIGNED_BYTE, GL_TRUE, 0, NULL);
+    glEnableVertexAttribArray(1);
+
+    GLint angleLocation = glGetUniformLocation(program, "angle");
+
+    ///////////////////
+    // Display window
+    ///////////////////
+
+    ShowWindow(window, showWindow);
+
+    //////////////////////////////////
+    // Start render and message loop
+    //////////////////////////////////
+
+    MSG message;
+    float angle = 0.0f;
+    bool running = true;
+    while (running) {
+        while (PeekMessage(&message, NULL, 0, 0, PM_REMOVE)) {
+            TranslateMessage(&message);
+            DispatchMessage(&message);
+
+            if (message.message == WM_QUIT) {
+                running = false; 
+                break;
+            }
+        }
+
+        setUniform(angleLocation, angle);
+        angle += 0.01;
+
+        if (angle > 2 * M_PI) {
+            angle -= 2 * M_PI;
+        }
+
+        glClear(GL_COLOR_BUFFER_BIT);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        SwapBuffers(deviceContext);
+    }
+
+    return (int) message.wParam;
+}
