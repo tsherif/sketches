310a55edf1970f890acb9be0eb97fcce459deb38
Tarek Sherif <tareksherif@pm.me>
2021-07-19 07:03:24 -0400

    Controller

---
+++ 2021-07/19/input.h
@@ -0,0 +1,24 @@
+#ifndef _TYPES_H_
+#define _TYPES_H_
+
+typedef struct {
+	bool left;
+	bool right;
+	bool up;
+	bool down;
+	bool space;
+	bool ctrl;
+} Keyboard;
+
+typedef struct {
+    int x;
+    int y;
+    bool clicked;
+} Mouse;
+
+typedef struct {
+	float leftStickX;
+	float leftStickY;
+} Controller;
+
+#endif
