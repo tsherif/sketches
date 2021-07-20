43b47292e9e094bbd7814420f98495962909a958
Tarek Sherif <tareksherif@pm.me>
2021-07-20 05:54:33 -0400

    Better gamepad diffing

---
+++ 2021-07/20/input.h
@@ -0,0 +1,25 @@
+#ifndef _TYPES_H_
+#define _TYPES_H_
+
+typedef struct {
+	bool left;
+	bool right;
+	bool up;
+	bool down;
+	bool space;
+	bool ctrl;
+	bool changed;
+} Keyboard;
+
+typedef struct {
+    int x;
+    int y;
+    bool clicked;
+} Mouse;
+
+typedef struct {
+	float leftStickX;
+	float leftStickY;
+} Controller;
+
+#endif
