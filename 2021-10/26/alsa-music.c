6c314d8610189dd70108ce132346292973dd8c17
Tarek Sherif <tareksherif@pm.me>
2021-10-26 06:21:45 -0400

    Audio mixing

--- 2021-10/26/alsa-music.c
+++ 2021-10/26/alsa-music.c
@@ -1,23 +1,56 @@
 #include <alsa/asoundlib.h>
 #include "../../lib/utils.h"
 #include "../../lib/linux-utils.h"
- 
+
+#define SAMPLE_MAX 32767
+#define SAMPLE_MIN (-32768)
+
+void mixSounds(int16_t* dst, int16_t* src, int32_t numFrames) {
+    for (int32_t i = 0; i < numFrames * 2; ++i) {
+        int32_t sample = dst[i] + src[i];
+        if (sample > SAMPLE_MAX) {
+            sample = SAMPLE_MAX;
+        }
+        if (sample < SAMPLE_MIN) {
+            sample = SAMPLE_MIN;
+        }
+
+        dst[i] = sample;
+    }
+}
+
 int main(void) {
     
     Buffer musicFile;
     Buffer musicData;
 
     if (!loadBinFile("../../audio/music.wav", &musicFile)) {
-        debugLog("Could not open sound file.");
+        debugLog("Could not open music file.");
         return 1;    
     }
     
     if (!wavToSound(&musicFile, &musicData)) {
+        debugLog("Could not parse music file.");
+        return 1;    
+    }
+
+    Buffer jumpFile;
+    Buffer jumpData;
+
+    if (!loadBinFile("../../audio/jump.wav", &jumpFile)) {
+        debugLog("Could not open sound file.");
+        return 1;    
+    }
+    
+    if (!wavToSound(&jumpFile, &jumpData)) {
         debugLog("Could not parse sound file.");
         return 1;    
     }
 
-    uint32_t numFrames = musicData.size / 4;
+    uint32_t numMusicFrame = musicData.size / 4;
+    uint32_t numJumpFrame = jumpData.size / 4;
+
+    mixSounds((int16_t*)(musicData.data + 4 * 44000), (int16_t*) jumpData.data, numJumpFrame);
 
     snd_pcm_t* device;
 
@@ -31,7 +64,7 @@ int main(void) {
         return 1;   
     }
 
-    if (snd_pcm_writei(device, musicData.data, numFrames) < 0) {
+    if (snd_pcm_writei(device, musicData.data, numMusicFrame) < 0) {
         debugLog("Could not play audio.");
         return 1;
     }
