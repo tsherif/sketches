25e39e76a8e60c000ba8afc1049992ba0da0c7bd
Tarek Sherif <tareksherif@pm.me>
2022-09-02 09:56:18 -0400

    Remove exception handling

--- 2022-09/02/build.bat
+++ 2022-09/02/build.bat
@@ -1,2 +1,2 @@
-cl /W4 /WX /wd5105 /wd4201 /wd4100 /wd4996 /wd4189 /Zi /RTCsu /EHsc gltf.c user32.lib gdi32.lib opengl32.lib
+cl /W4 /WX /wd5105 /wd4201 /wd4100 /wd4996 /wd4189 /Zi /RTCsu gltf.c user32.lib gdi32.lib opengl32.lib
  
