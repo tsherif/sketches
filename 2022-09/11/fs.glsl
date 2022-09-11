888a9a47009d52a45ac283d4c590c88de0b89403
Tarek Sherif <tareksherif@pm.me>
2022-09-11 07:59:52 -0400

    Lighting calculations in tangent space

--- 2022-09/11/fs.glsl
+++ 2022-09/11/fs.glsl
@@ -1,52 +1,45 @@
 #version 440
-layout(std140, column_major) uniform SceneUniforms {
-    mat4 proj;
-    vec4 eyePosition;
-    vec4 lightPosition;
-    vec4 lightDirection;
-    vec4 viewLightPosition;
-};
 
 uniform mat4 view;
 uniform sampler2D tex;
 uniform sampler2D normalMap;
 
-in vec3 vPosition;
-in mat3 vTBN;
+in vec3 vTangentEyeVec;
+in vec3 vTangentLightVec;
+in vec3 vTangentLightDir;
+in vec3 vTangentHeadlightVec;
 in vec2 vUV;
 
 out vec4 fragColor;
 
 void main() {
     vec3 color = texture(tex, vUV).rgb;
-    vec3 normal = texture(normalMap, vUV).xyz * 2.0 - 1.0;
-    normal = normalize(vTBN * normal);
+    vec3 tangentNormal = texture(normalMap, vUV).xyz * 2.0 - 1.0;
 
-    mat3 view3 = mat3(view);
-
-    vec3 eyeVec = view3 * normalize(eyePosition.xyz - vPosition);
-    vec3 lightVec = view3 * normalize(lightPosition.xyz - vPosition);
-    vec3 viewLightDir = view3 * normalize(-lightDirection.xyz);
+    vec3 tangentEyeVec = normalize(vTangentEyeVec);
+    vec3 tangentLightVec = normalize(vTangentLightVec);
+    vec3 tangentLightDir = normalize(vTangentLightDir);
+    vec3 tangentHeadlightVec = normalize(vTangentHeadlightVec);
     
     vec3 diffuse = vec3(0.0);
     vec3 highlight = vec3(0.0);
+    vec3 ambient = vec3(0.0);
     vec3 lightColor = vec3(0.0);
+    vec3 halfway = vec3(0.0);
 
     lightColor = vec3(0.4);
-    vec3 halfway = normalize(eyeVec + lightVec);
-    diffuse += lightColor * max(dot(lightVec, normal), 0.0);
-    highlight += lightColor * pow(max(dot(halfway, normal), 0.0), 100.0);
+    halfway = normalize(tangentEyeVec + tangentLightVec);
+    diffuse += lightColor * max(dot(tangentLightVec, tangentNormal), 0.0);
+    highlight += lightColor * pow(max(dot(halfway, tangentNormal), 0.0), 100.0);
 
-    vec4 viewPosition = view * vec4(vPosition, 1.0);
     lightColor = vec3(0.7);
-    lightVec = normalize(viewLightPosition.xyz - viewPosition.xyz);
-    halfway = normalize(eyeVec + lightVec);
-    diffuse += lightColor * max(dot(lightVec, normal), 0.0);
-    highlight += lightColor * pow(max(dot(halfway, normal), 0.0), 100.0);
+    halfway = normalize(tangentEyeVec + tangentHeadlightVec);
+    diffuse += lightColor * max(dot(tangentHeadlightVec, tangentNormal), 0.0);
+    highlight += lightColor * pow(max(dot(halfway, tangentNormal), 0.0), 100.0);
 
     lightColor = vec3(0.4);
-    diffuse += lightColor * max(dot(normalize(viewLightDir), normal), 0.0);;
+    diffuse += lightColor * max(dot(-tangentLightDir, tangentNormal), 0.0);
     
-    float ambient = 0.1;
+    ambient += 0.1;
     fragColor = vec4(color * (diffuse + highlight + ambient), 1.0);
 };
--- 2022-09/11/vs.glsl
+++ 2022-09/11/vs.glsl
@@ -11,28 +11,44 @@ layout(std140, column_major) uniform SceneUniforms {
     vec4 eyePosition;
     vec4 lightPosition;
     vec4 lightDirection;
-    vec4 viewLightPosition;
-};       
+    vec4 headlightPosition;
+};
+
 uniform mat4 view;
 uniform mat4 world;
 
-out  vec3 vPosition;
-out  mat3 vTBN;
-out  vec2 vUV;
+out vec3 vTangentEyeVec;
+out vec3 vTangentLightVec;
+out vec3 vTangentLightDir;
+out vec3 vTangentHeadlightVec;
+out vec2 vUV;
 
 void main() {
-    vec4 worldPosition = world * position;
-    
-    vPosition = worldPosition.xyz;
     vUV = uv;
     
+    vec3 worldNormal = mat3(world) * normal;
     vec3 bitangent = cross(normal, tangent);
-    mat3 view3 = mat3(view) * mat3(world);
-    vTBN =  mat3 (
-        normalize(view3 * tangent),
-        normalize(view3 * bitangent),
-        normalize(view3 * normal)
+    mat3 inverseTBN =  transpose(
+        mat3(
+            tangent,
+            bitangent,
+            worldNormal
+        )
     );
 
-    gl_Position = proj * view * worldPosition;
+    vec4 worldPosition = world * position;
+    vec3 worldEyeVec = normalize(eyePosition.xyz - worldPosition.xyz);
+    vec3 worldLightVec = normalize(lightPosition.xyz - worldPosition.xyz);
+    vec3 worldLightDir = normalize(lightDirection.xyz);
+
+    mat3 inverseView = transpose(mat3(view));
+    vec4 viewPosition = view * worldPosition;
+    vec3 worldHeadlightVec = inverseView * normalize(headlightPosition.xyz - viewPosition.xyz);
+
+    vTangentEyeVec = inverseTBN * worldEyeVec;
+    vTangentLightVec = inverseTBN * worldLightVec;
+    vTangentLightDir = inverseTBN * worldLightDir;
+    vTangentHeadlightVec = inverseTBN * worldHeadlightVec;
+
+    gl_Position = proj * viewPosition;
 };
