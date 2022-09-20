cbff0435da9f0e7f4560bcca7d4de3f1495a5625
Tarek Sherif <tareksherif@pm.me>
2022-09-20 07:50:52 -0400

    Draw a sphere

---
+++ 2022-09/20/vs.glsl
@@ -0,0 +1,25 @@
+#version 440
+
+layout(std140, column_major) uniform;
+layout(location=0) in vec4 position;
+layout(location=1) in vec3 normal;
+
+layout(std140, column_major) uniform SceneUniforms {
+    mat4 proj;
+    vec4 eyePosition;
+    vec4 lightPosition;
+    vec4 lightDirection;
+};       
+uniform mat4 view;
+uniform mat4 world;
+
+out  vec3 vPosition;
+out  vec2 vUV;
+out  vec3 vNormal;
+
+void main() {
+    vec4 worldPosition = position;
+    vPosition = worldPosition.xyz;
+    vNormal = normal;
+    gl_Position = proj * view * worldPosition;
+};
