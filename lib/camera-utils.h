aceda5c0631a3a813061defe39ab36e540916874
Tarek Sherif <tareksherif@pm.me>
2022-09-26 06:17:28 -0400

    Add panning to camera

--- lib/camera-utils.h
+++ lib/camera-utils.h
@@ -11,7 +11,6 @@ void orbitCamera(hmm_vec3* pEye, hmm_vec3* pLook, hmm_vec3* pUp, float x, float 
     up = HMM_Cross(eye, axis);
     up = HMM_NormalizeVec3(up);
 
-    // HMM_Rotate
     hmm_mat4 xRotation = HMM_Rotate(x, up);
     hmm_mat4 yRotation = HMM_Rotate(y, axis);
     hmm_mat4 rotation = HMM_MultiplyMat4(xRotation, yRotation);
@@ -23,6 +22,27 @@ void orbitCamera(hmm_vec3* pEye, hmm_vec3* pLook, hmm_vec3* pUp, float x, float 
     *pEye = eye;
     *pLook = look;
     *pUp = up;
+}
+
+void panCamera(hmm_vec3* pEye, hmm_vec3* pLook, hmm_vec3* pUp, float x, float y) {
+    hmm_vec3 eye = *pEye;
+    hmm_vec3 look = *pLook;
+    hmm_vec3 up = *pUp;
+
+    hmm_vec3 forward = HMM_SubtractVec3(look, eye);
+    hmm_vec3 right = HMM_Cross(forward, up);
+    right = HMM_NormalizeVec3(right);
+
+    hmm_vec3 delta = HMM_AddVec3(
+        HMM_MultiplyVec3f(right, x),
+        HMM_MultiplyVec3f(up, y)
+    );
+
+    eye = HMM_AddVec3(eye, delta);
+    look = HMM_AddVec3(look, delta);
+
+    *pEye = eye;
+    *pLook = look;
 }
 
 void zoomCamera(hmm_vec3* pEye, hmm_vec3* pLook, float zoom) {
