c9e81d944da268a67624c871afd21846df66588b
Tarek Sherif <tareksherif@pm.me>
2023-01-28 09:10:11 -0500

    Sim running in thread

--- 2023-01/28/sim-worker.js
+++ 2023-01/28/sim-worker.js
@@ -1,7 +1,45 @@
 addEventListener("message", message => {
-    const {start, end, buffer} = message.data;
+    const {start, end, buffer, width, height} = message.data;
 
-    console.log(start, end, buffer);
+    const balls = new Float32Array(buffer);
 
-    postMessage({});
+    for (let i = start; i <= end; ++i) {
+        const bi = i * 5;
+        let x =  balls[bi];
+        let y =  balls[bi + 1];
+        let vx = balls[bi + 2];
+        let vy = balls[bi + 3];
+        let r =  balls[bi + 4];
+
+        x += vx;
+        y += vy;
+
+        if (x < r) {
+            x = r;
+            vx *= -1;
+        }
+
+        if (x > width - r) {
+            x = width - r;
+            vx *= -1;
+        }
+
+        if (y < r) {
+            y = r;
+            vy *= -1;
+        }
+
+        if (y > height - r) {
+            y = height - r;
+            vy *= -1;
+        }
+
+        balls[bi]     = x;
+        balls[bi + 1] = y;
+        balls[bi + 2] = vx;
+        balls[bi + 3] = vy;
+        balls[bi + 4] = r;
+    }
+
+    postMessage("done");
 })
