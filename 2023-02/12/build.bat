3c1ddc31cea656296669dd650cf6d3f168075c01
Tarek Sherif <tareksherif@pm.me>
2023-02-12 06:02:13 -0500

    Switch to strings

---
+++ 2023-02/12/build.bat
@@ -0,0 +1,3 @@
+
+cl /DSOGL_MAJOR_VERSION=4 /DSOGL_MAJOR_VERSION=4 /W4 /WX /wd5105 /wd4201 /wd4100 /wd4996 /wd4189 /Zi /EHsc /RTCsu pbr-gltf.cpp ..\..\lib\gl-utils\lib\utils.c ..\..\lib\gl-utils\cpp\vertex-array.cpp ..\..\lib\gl-utils\cpp\buffer.cpp user32.lib gdi32.lib opengl32.lib
+ 
