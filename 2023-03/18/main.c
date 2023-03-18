6ae608cc4b860bfb017a8a7d44c25cbb448e933f
Tarek Sherif <tareksherif@pm.me>
2023-03-18 07:12:47 -0400

    Test map

--- 2023-03/18/main.c
+++ 2023-03/18/main.c
@@ -15,6 +15,11 @@
 #define IMPLEMENTATION
 #include "../../lib/c/vector.h"
 
+#define KEY_TYPE int
+#define TYPE float
+#define IMPLEMENTATION
+#include "../../lib/c/map.h"
+
 const char* words[] = {
     "Development",
     "on",
@@ -485,6 +490,44 @@ void main(void) {
             printf("Using Nested RES2\n");
         }
     }
+
+    Map_int_float m = Map_int_float_create();
+
+    printf("Map is empty: %d\n", Map_int_float_empty(&m));
+
+    printf("Adding entries.\n");
+    Map_int_float_set(&m, 1, 1.5);    
+    Map_int_float_set(&m, 2, 2.5);    
+    Map_int_float_set(&m, 3, 3.5);    
+    Map_int_float_set(&m, 4, 5.5);    
+    Map_int_float_set(&m, 2, 6.5);    
+    Map_int_float_set(&m, 3, 7.5);    
+    Map_int_float_set(&m, 2, 8.5);    
+    Map_int_float_set(&m, 1, 9.5);
+    printf("Map size: %zd\n", Map_int_float_size(&m));
+    printf("Map capacity: %zd\n", m.capacity);
+
+    for (size_t i = 0; i < m.capacity; ++i) {
+        if (m.entries[i].active) {
+            printf("%d = %f\n", m.entries[i].key, Map_int_float_get(&m, m.entries[i].key));
+        }
+    }
+
+    printf("Adding more entries.\n");
+    for (int i = 0; i < 40; ++i) {
+        Map_int_float_set(&m, i, i + 0.5f);    
+    }
+    printf("Map size: %zd\n", Map_int_float_size(&m));
+    printf("Map capacity: %zd\n", m.capacity);
+
+    for (int i = 0; i < 40; ++i) {
+        printf("%d = %f\n", i, Map_int_float_get(&m, i));
+    }
+
+    Map_int_float_clear(&m);
+    printf("Clearing map.\n");
+    printf("Map size: %zd\n", Map_int_float_size(&m));
+    printf("Map is empty: %d\n", Map_int_float_empty(&m));
 
     MY_VEC myv = MY_VEC_create();
     for (int i = 0; i < 50; ++i) {
--- lib/c/map.h
+++ lib/c/map.h
@@ -15,21 +15,21 @@ typedef struct {
     TYPE value;
     uint64_t hash;
     bool active;
-} MAP_PRIVATE(_entry);
+} PRIVATE(_entry);
 
 typedef struct {
-    MAP_PRIVATE(_entry)* entries;
+    PRIVATE(_entry)* entries;
     size_t size;
     size_t capacity;
-} MAP_CONTAINER;
+} CONTAINER;
 
-MAP_CONTAINER MAP_METHOD(_create)(void);
-void MAP_METHOD(_set)(MAP_CONTAINER* h, KEY_TYPE key, TYPE value);
-TYPE MAP_METHOD(_get)(MAP_CONTAINER* h, KEY_TYPE key);
-void MAP_METHOD(_clear)(MAP_CONTAINER* h);
-size_t MAP_METHOD(_size)(MAP_CONTAINER* h);
-bool MAP_METHOD(_empty)(MAP_CONTAINER* h);
-void MAP_METHOD(_destroy)(MAP_CONTAINER* h);
+CONTAINER METHOD(_create)(void);
+void METHOD(_set)(CONTAINER* h, KEY_TYPE key, TYPE value);
+TYPE METHOD(_get)(CONTAINER* h, KEY_TYPE key);
+void METHOD(_clear)(CONTAINER* h);
+size_t METHOD(_size)(CONTAINER* h);
+bool METHOD(_empty)(CONTAINER* h);
+void METHOD(_destroy)(CONTAINER* h);
 
 #ifdef IMPLEMENTATION
 
@@ -40,7 +40,7 @@ void MAP_METHOD(_destroy)(MAP_CONTAINER* h);
 #define INITIAL_SIZE 32
 #endif
 
-uint64_t MAP_PRIVATE(_fnv1aHash)(void* key, size_t len) {
+uint64_t PRIVATE(_fnv1aHash)(void* key, size_t len) {
     const char* curr = key;
     uint64_t hash = 0x00000100000001B3ULL;
 
@@ -52,15 +52,15 @@ uint64_t MAP_PRIVATE(_fnv1aHash)(void* key, size_t len) {
     return hash;
 }
 
-void MAP_PRIVATE(_extend)(MAP_CONTAINER* h) {
+void PRIVATE(_extend)(CONTAINER* h) {
     size_t newCapacity = h->capacity * 2;
-    MAP_PRIVATE(_entry)* newEntries = calloc(newCapacity, sizeof(MAP_PRIVATE(_entry)));
+    PRIVATE(_entry)* newEntries = calloc(newCapacity, sizeof(PRIVATE(_entry)));
     for (size_t i = 0; i < h->capacity; ++i) {
-        MAP_PRIVATE(_entry) *oldEntry = h->entries + i;
+        PRIVATE(_entry) *oldEntry = h->entries + i;
         if (oldEntry->active) {
             size_t mask = newCapacity - 1;
             size_t newIndex = oldEntry->hash & mask;
-            MAP_PRIVATE(_entry)* newEntry = newEntries + newIndex;
+            PRIVATE(_entry)* newEntry = newEntries + newIndex;
             while (newEntry->active && newEntry->key != oldEntry->key) {
                 newIndex = (newIndex + 1) & mask;
                 newEntry = newEntries + newIndex;
@@ -74,12 +74,12 @@ void MAP_PRIVATE(_extend)(MAP_CONTAINER* h) {
     h->entries = newEntries;
 }
 
-size_t MAP_PRIVATE(_probe)(MAP_CONTAINER* h, KEY_TYPE key, uint64_t hash) {
+size_t PRIVATE(_probe)(CONTAINER* h, KEY_TYPE key, uint64_t hash) {
     size_t mask = h->capacity - 1;
     
     size_t index = hash & mask;
 
-    MAP_PRIVATE(_entry) *e = h->entries + index;
+    PRIVATE(_entry) *e = h->entries + index;
     while (e->active && e->key != key) {
         index = (index + 1) & mask;
         e = h->entries + index;
@@ -88,16 +88,16 @@ size_t MAP_PRIVATE(_probe)(MAP_CONTAINER* h, KEY_TYPE key, uint64_t hash) {
     return index;
 }
 
-MAP_CONTAINER MAP_METHOD(_create)(void) {
-    return (MAP_CONTAINER) {
-        .entries = calloc(INITIAL_SIZE, sizeof(MAP_PRIVATE(_entry))),
+CONTAINER METHOD(_create)(void) {
+    return (CONTAINER) {
+        .entries = calloc(INITIAL_SIZE, sizeof(PRIVATE(_entry))),
         .capacity = INITIAL_SIZE,
     };
 }
 
-void MAP_METHOD(_set)(MAP_CONTAINER* h, KEY_TYPE key, TYPE value) {
-    uint64_t hash = MAP_PRIVATE(_fnv1aHash)(&key, sizeof(key));
-    MAP_PRIVATE(_entry)* e = h->entries + MAP_PRIVATE(_probe)(h, key, hash);
+void METHOD(_set)(CONTAINER* h, KEY_TYPE key, TYPE value) {
+    uint64_t hash = PRIVATE(_fnv1aHash)(&key, sizeof(key));
+    PRIVATE(_entry)* e = h->entries + PRIVATE(_probe)(h, key, hash);
 
     e->key = key;
     e->value = value;
@@ -107,31 +107,31 @@ void MAP_METHOD(_set)(MAP_CONTAINER* h, KEY_TYPE key, TYPE value) {
         e->active = true;
         ++h->size;
         if (h->size > h->capacity / 2) {
-            MAP_PRIVATE(_extend)(h);
+            PRIVATE(_extend)(h);
         }
     }
 }
 
-TYPE MAP_METHOD(_get)(MAP_CONTAINER* h, KEY_TYPE key) {
-    uint64_t hash = MAP_PRIVATE(_fnv1aHash)(&key, sizeof(key));
-    MAP_PRIVATE(_entry)* e = h->entries + MAP_PRIVATE(_probe)(h, key, hash);
+TYPE METHOD(_get)(CONTAINER* h, KEY_TYPE key) {
+    uint64_t hash = PRIVATE(_fnv1aHash)(&key, sizeof(key));
+    PRIVATE(_entry)* e = h->entries + PRIVATE(_probe)(h, key, hash);
 
     return e->value;
 }
 
-size_t MAP_METHOD(_size)(MAP_CONTAINER* h) {
+size_t METHOD(_size)(CONTAINER* h) {
     return h->size;
 }
 
-bool MAP_METHOD(_empty)(MAP_CONTAINER* h) {
+bool METHOD(_empty)(CONTAINER* h) {
     return h->size == 0;
 }
 
-void MAP_METHOD(_clear)(MAP_CONTAINER* h) {
+void METHOD(_clear)(CONTAINER* h) {
     h->size = 0;
 }
 
-void MAP_METHOD(_destroy)(MAP_CONTAINER* h) {
+void METHOD(_destroy)(CONTAINER* h) {
     if (h->entries) {
         free(h->entries);
     }
