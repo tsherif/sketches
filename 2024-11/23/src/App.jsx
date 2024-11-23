efe71a35e2e27b42b36bf10b9eed2c895f5f0569
Tarek Sherif <tareksherif@pm.me>
2024-11-23 16:27:08 -0500

    React Three Fiber Test

--- 2024-11/23/src/App.jsx
+++ 2024-11/23/src/App.jsx
@@ -1,5 +1,32 @@
-import React from "react";
+import React, { useRef } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+
+function Box() {
+    const meshRef = useRef();
+
+    useFrame((state, delta) => {
+        meshRef.current.rotation.x += delta;
+        meshRef.current.rotation.y += 0.5 * delta;
+    });
+
+    return (
+        <mesh ref={meshRef}>
+            <boxGeometry args={[1, 1, 1]} />
+            <meshStandardMaterial color="white" />
+        </mesh>
+    )
+}
 
 export function App() {
-    return <div>Hello!</div>
+    
+
+    return (
+        <Canvas>
+            <ambientLight intensity={0.1} />
+            <pointLight position={[1, 1, 1]} color="red" />
+            <pointLight position={[-1, 1, 1]} color="blue" />
+            <pointLight position={[0, -1, 1]} color="green" />
+            <Box />
+        </Canvas>
+    )
 }
