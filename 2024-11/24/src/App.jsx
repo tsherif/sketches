b89dbff34cfbc1b1cecedbd1c44cee3440d2757e
Tarek Sherif <tareksherif@pm.me>
2024-11-24 16:58:56 -0500

    More objects

---
+++ 2024-11/24/src/App.jsx
@@ -0,0 +1,60 @@
+import React, { useRef, useMemo } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+
+function Box({position, vxr, vyr, scale}) {
+    const meshRef = useRef();
+
+    useFrame((state, delta) => {
+        meshRef.current.rotation.x += vxr * delta;
+        meshRef.current.rotation.y += vyr * delta;
+    });
+
+    return (
+        <mesh ref={meshRef} position={position} scale={[scale, scale, scale]}>
+            <boxGeometry args={[1, 1, 1]} />
+            <meshStandardMaterial color="white" />
+        </mesh>
+    )
+}
+
+export function App() {
+
+    const boxes = useMemo(() => (
+        new Array(100).fill().map(() => ({
+            position: [
+                Math.random() * 6 - 3,
+                Math.random() * 6 - 3,
+                Math.random() * 6 - 3
+            ],
+            vxr: Math.random() * 4 - 2,
+            vyr: Math.random() * 4 - 2,
+            scale: Math.random() + 0.1
+        })) 
+    ), []);
+
+    const lights = useMemo(() => (
+        new Array(10).fill().map(() => ({
+            position: [
+                Math.random() * 6 - 3,
+                Math.random() * 6 - 3,
+                Math.random() * 6 - 3
+            ],
+            color: ["red", "green", "blue"][Math.floor(Math.random() * 3)]
+        })) 
+    ), []);
+    
+
+    return (
+        <Canvas background="black">
+            <ambientLight intensity={0.1} />
+            {
+                lights.map(l => (
+                    <pointLight {...l} />
+                ))
+            }
+            {boxes.map(b => (
+                <Box {...b} />
+            ))}
+        </Canvas>
+    )
+}
