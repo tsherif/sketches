79ed816f2b99ca9d0dd0c34edbc35cef40bb97b0
Tarek Sherif <tareksherif@pm.me>
2024-11-29 20:09:28 -0500

    useFBO

--- 2024-11/29b/src/App.jsx
+++ 2024-11/29b/src/App.jsx
@@ -1,25 +1,48 @@
 import React, { useLayoutEffect, useRef, useState } from "react";
-import { Canvas, useLoader, useThree } from "@react-three/fiber";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-import { CameraControls } from "@react-three/drei";
+import { Canvas, useFrame, useLoader, useThree } from "@react-three/fiber";
+import { TextureLoader } from 'three/src/loaders/TextureLoader';
+import { CameraControls, useFBO } from "@react-three/drei";
 
 function Scene() {
-    const model = useLoader(GLTFLoader, "models/antique-camera/glTF/AntiqueCamera.gltf");
+    const backgroundRef = useRef();
+    const meshRef = useRef();
+    const materialRef = useRef();
+    const texture = useLoader(TextureLoader, "img/webgl-logo.png");
+    const fbo = useFBO();
+
+    useFrame(({gl, scene, camera}) => {
+        meshRef.current.rotation.x += 0.01;
+        meshRef.current.rotation.y += 0.02;
+
+        gl.setRenderTarget(fbo);
+        backgroundRef.current.set("gray");
+        materialRef.current.map = texture;
+        gl.render(scene, camera);
+
+        gl.setRenderTarget(null);
+        backgroundRef.current.set("black");
+        materialRef.current.map = fbo.texture;
+        gl.render(scene, camera);
+    });
 
     return (
         <>
-            <color args={['gray']} attach="background" />
-            <ambientLight intensity={0.1} />
-            <pointLight position={[5, 5, 5]} color="white" decay={0} intensity={5} />
-            <pointLight position={[-5, 5, -5]} color="white" decay={0} intensity={5} />
-            <primitive position={[0, -4, 0]} object={model.scene} />
+            <color ref={backgroundRef} attach="background" />
+            <ambientLight intensity={0.2} />
+            <pointLight position={[1, 1, 1]} color="white" decay={0} />
+            <pointLight position={[-1, 1, -1]} color="white" decay={0} />
+            <pointLight position={[0, -1, 0]} color="white" decay={0} />
+            <mesh ref={meshRef}>
+                <boxGeometry args={[1, 1, 1]} />
+                <meshPhongMaterial ref={materialRef} specular={0xFFFFFF} shininess={100} />
+            </mesh>
         </>
     )
 }
 
 export function App() {
     return (
-        <Canvas camera={{fov: 90, near: 0.1, far: 30, position: [0, 0, 7]}} >
+        <Canvas camera={{fov: 90, near: 0.1, far: 10, position: [1, 1, 1]}}>
             <CameraControls />
             <Scene /> 
         </Canvas>
